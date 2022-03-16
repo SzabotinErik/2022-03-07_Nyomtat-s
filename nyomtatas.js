719cf368571a7650a9575cc96608eeb2e9ef9f4e
SzabotinErik <szabotine.12d@acsjszki.hu>
2022-03-16 11:10:50 +0100

    7.b

--- nyomtatas.js
+++ nyomtatas.js
@@ -7,6 +7,9 @@ function kalkulal(){
     let terulet=Math.round((szelesseg*magassag)/10000);    
     let koltseg=terulet*papir;
     //Megjelenítés
+    document.getElementById('terulet').innerHTML=terulet;
+    document.getElementById('papir').innerHTML=papir;
+    document.getElementById('koltseg').innerHTML=koltseg;
     document.getElementById('valasz').style.visibility = "visible";
 }
 
